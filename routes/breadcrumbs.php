8ac6c21ef129f2530dd8d52ded90d496fb5df296
Fatur Maulidan <mfatur757@gmail.com>
2024-05-17 19:54:44 +0700

    Menambahkan breadcrumb diglactic pada home kaprodi

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -4,3 +4,8 @@ use App\Models\User;
 use Diglactic\Breadcrumbs\Breadcrumbs;
 use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
 
+Breadcrumbs::for('kurikulum.index', function (BreadcrumbTrail $trail): void {
+    $trail->push('Home', route('kurikulum.index'));
+});
+
+?>
