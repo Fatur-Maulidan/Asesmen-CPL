d66731b4fb1b866193e2ce21a0b4123c52c993e8
Fatur Maulidan <mfatur757@gmail.com>
2024-05-18 21:13:11 +0700

    Setup Select2 for dropdown

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,6 +16,9 @@ mix.js('resources/js/app.js', 'public/js')
         //
     ]);
 
-mix.sass('resources/sass/app.scss', 'public/css')
+mix.sass('resources/sass/app.scss', 'public/css');
 
 mix.browserSync('127.0.0.1:8000');
+
+mix.js('resources/js/app.js', 'public/js')    
+.sass('resources/sass/app.scss', 'public/css');
