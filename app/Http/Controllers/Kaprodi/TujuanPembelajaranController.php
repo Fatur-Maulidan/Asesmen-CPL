804a1ad50406377bd097da015f91269d620a1435
Ananta Destawardhana <anantadesta.public@gmail.com>
2024-05-28 09:31:21 +0700

    fix kaprodi/tujuanpembeljaran controller

--- app/Http/Controllers/Kaprodi/TujuanPembelajaranController.php
+++ app/Http/Controllers/Kaprodi/TujuanPembelajaranController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Kaprodi;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class TPController extends Controller
+class TujuanPembelajaranController extends Controller
 {
     /**
      * Display a listing of the resource.
