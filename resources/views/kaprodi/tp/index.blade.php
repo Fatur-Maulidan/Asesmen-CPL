5e474ee900be0daa5ebc7a4d2e6620b701553570
Fatur Maulidan <mfatur757@gmail.com>
2024-06-06 09:12:08 +0700

    Memperbaiki cara mengambil data untuk kurikulum pada Tujuan Pembelajaran di Kaprodi

--- resources/views/kaprodi/tp/index.blade.php
+++ resources/views/kaprodi/tp/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('breadcrumb')
-    {{ Breadcrumbs::render('kaprodi.tp.index', $kurikulum) }}
+    {{ Breadcrumbs::render('kaprodi.tp.index', $kurikulum->tahun) }}
     <h1 class="fw-bold mb-0">{{ $title }}</h1>
 @endsection
 
